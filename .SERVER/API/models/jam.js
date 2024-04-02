f94126f7bf31e9a9884761e57d684b9df1da2bad
Ruslan-Cherniavsky <Nalsurion@gmail.com>
2024-04-02 18:32:05 +0300

    added jam model to server

---
+++ .SERVER/API/models/jam.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose")
+
+const jamSchema = mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  img: {type: String},
+  jamName: {type: String, maxlength: 30},
+  hostedBy: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
+  jamDate: {type: Date},
+  country: {type: String, maxlength: 60},
+  isoCode: {type: String, maxlength: 60},
+  city: {type: String, maxlength: 90},
+  region: {type: String, maxlength: 130},
+  type: {type: String},
+  entrance: {type: String},
+  tune: {type: String},
+  jamDescription: {type: String, maxlength: 700},
+  genres: [{type: mongoose.Schema.Types.ObjectId, ref: "Genres"}],
+  sharedInstruments: [
+    {type: mongoose.Schema.Types.ObjectId, ref: "Instruments"},
+  ],
+  jammers: [
+    {
+      instrument: {type: mongoose.Schema.Types.ObjectId, ref: "Instruments"},
+      maxNumberOfJammers: {type: Number},
+      jammersId: [{type: mongoose.Schema.Types.ObjectId, ref: "User"}],
+    },
+  ],
+  audience: [{type: mongoose.Schema.Types.ObjectId, ref: "User"}],
+  reports: [
+    {
+      reporter: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
+      reason: String,
+      date: {type: Date, default: Date.now},
+    },
+  ],
+  createdAt: {type: Date, immutable: true, default: Date.now},
+})
+
+module.exports = mongoose.model("Jam", jamSchema)
