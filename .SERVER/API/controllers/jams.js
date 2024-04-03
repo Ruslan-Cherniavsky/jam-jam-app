b25ccbc296c29c036d5676738aacc245a6c13636
Ruslan-Cherniavsky <Nalsurion@gmail.com>
2024-04-03 12:15:10 +0300

    server added a jam logic 2

--- .SERVER/API/controllers/jams.js
+++ .SERVER/API/controllers/jams.js
@@ -63,6 +63,63 @@ const getAllJams = async (req, res) => {
   }
 }
 
+const getJamById = async (req, res) => {
+  const jamId = req.params.jamid
+
+  try {
+    const jam = await Jam.findById(jamId).populate(
+      "hostedBy genres sharedInstruments jammers.instrument audience reports.reporter"
+    )
+
+    if (!jam) {
+      return res.status(404).json({message: "Jam not found!"})
+    }
+
+    return res.status(200).json({jam})
+  } catch (error) {
+    console.error("Error fetching jam by ID:", error)
+    res.status(500).json({error})
+  }
+}
+
+const updateJam = async (req, res) => {
+  const jamId = req.params.jamid
+
+  try {
+    const jam = await Jam.findById(jamId)
+
+    if (!jam) {
+      return res.status(404).json({message: "Jam not found!"})
+    }
+
+    await Jam.updateOne({_id: jamId}, req.body)
+
+    return res.status(200).json({message: "Jam updated"})
+  } catch (error) {
+    console.error("Error updating jam:", error)
+    res.status(500).json({error})
+  }
+}
+
+const deleteJamById = async (req, res) => {
+  const jamId = req.params.jamId
+
+  try {
+    const jam = await Jam.findById(jamId)
+
+    if (!jam) {
+      return res.status(404).json({message: "Jam not found!"})
+    }
+
+    await Jam.deleteOne({_id: jamId})
+
+    return res.status(200).json({message: `Jam with id: ${jamId} deleted`})
+  } catch (error) {
+    console.error("Error deleting jam:", error)
+    res.status(500).json({error})
+  }
+}
+
 const getAllJamsWithPagination = async (req, res) => {
   const page = parseInt(req.query.page) || 1
   const perPage = 12
@@ -123,6 +180,22 @@ const getJamsFiltered = async (req, res) => {
       queryConditions.city = params.city
     }
 
+    //----
+
+    if (params.type) {
+      queryConditions.city = params.type
+    }
+
+    if (params.entrance) {
+      queryConditions.city = params.entrance
+    }
+
+    if (params.tune) {
+      queryConditions.city = params.tune
+    }
+
+    //----
+
     if (Object.keys(queryConditions).length === 0) {
       return res.status(400).json({error: "No search conditions provided."})
     }
@@ -144,9 +217,88 @@ const getJamsFiltered = async (req, res) => {
   }
 }
 
+const getAllJamsByHostedById = async (req, res) => {
+  const page = parseInt(req.query.page) || 1
+  const perPage = 12
+  const userId = req.params.userId
+
+  try {
+    const queryConditions = {hostedBy: userId}
+
+    const totalJams = await Jam.countDocuments(queryConditions)
+    const totalPages = Math.ceil(totalJams / perPage)
+
+    const jams = await Jam.find(queryConditions)
+      .skip((page - 1) * perPage)
+      .limit(perPage)
+    // .populate("hostedBy genres sharedInstruments jammers.instrument audience reports.reporter");
+
+    return res.status(200).json({jams, totalPages})
+  } catch (error) {
+    console.error("Error fetching jams by hosted user ID:", error)
+    res.status(500).json({error: "Internal server error."})
+  }
+}
+
+const getJamByJamName = async (req, res) => {
+  const page = parseInt(req.query.page) || 1
+  const perPage = 12
+  const searchText = req.query.jamName
+
+  try {
+    const totalJams = await Jam.countDocuments({
+      jamName: {$regex: new RegExp(searchText, "i")},
+    })
+    const totalPages = Math.ceil(totalJams / perPage)
+
+    const jams = await Jam.find({
+      jamName: {$regex: new RegExp(searchText, "i")},
+    })
+      .skip((page - 1) * perPage)
+      .limit(perPage)
+      .populate(
+        "hostedBy genres sharedInstruments jammers.instrument audience reports.reporter"
+      )
+
+    return res.json({jams, totalPages})
+  } catch (error) {
+    console.error("Error searching jams by jam name:", error)
+    res.status(500).json({error: "Internal server error."})
+  }
+}
+
+const reportJam = async (req, res) => {
+  try {
+    const {reportedJamId, userId, reason} = req.body
+
+    const jam = await Jam.findById(reportedJamId)
+    if (!jam) {
+      return res.status(404).json({message: "Reported jam not found."})
+    }
+
+    jam.reports.push({
+      reporter: userId,
+      reason,
+    })
+
+    await jam.save()
+
+    res.status(200).json({message: "Jam reported successfully."})
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({message: "Internal server error."})
+  }
+}
+
 module.exports = {
   createJam,
   getAllJams,
   getAllJamsWithPagination,
   getJamsFiltered,
+  getJamById,
+  updateJam,
+  deleteJamById,
+  reportJam,
+  getAllJamsByHostedById,
+  getJamByJamName,
 }
--- .SERVER/API/routes/jams.js
+++ .SERVER/API/routes/jams.js
@@ -5,6 +5,12 @@ const {
   getAllJams,
   getAllJamsWithPagination,
   getJamsFiltered,
+  getJamById,
+  updateJam,
+  deleteJamById,
+  reportJam,
+  getAllJamsByHostedById,
+  getJamByJamName,
 } = require("../controllers/jams")
 
 // POST endpoint to create a new Jam
@@ -12,5 +18,11 @@ router.post("/create", createJam)
 router.get("/getalljams", getAllJams)
 router.get("/getalljams-paginate", getAllJamsWithPagination)
 router.get("/getalljams-paginate-filtered", getJamsFiltered)
+router.get("/getjambyid/:jamid", getJamById)
+router.patch("/updatejam/:jamid", updateJam)
+router.delete("/deletebyid/:jamid", deleteJamById)
+router.post("/reportjam/", reportJam)
+router.get("/getalljamsbyhostedbyid/:userId", getAllJamsByHostedById)
+router.get("/getjambyjamname", getJamByJamName)
 
 module.exports = router
